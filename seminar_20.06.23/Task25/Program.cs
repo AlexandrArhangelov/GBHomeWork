c5e7ceb30e37ef0953c40a3166dd9567fef660c8
Alexandr S <zerro2007@mail.ru>
2023-06-25 19:10:26 +0300

    home work is done

---
+++ seminar_20.06.23/Task25/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int Print(string message){
+
+    Console.Write(message);
+    int readInput = Convert.ToInt32(Console.ReadLine());
+    int result = readInput;
+    return result;
+}
+
+int Power(int powerBase, int exponent){
+
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
+
+int powerBase = Print("Введите значение: ");
+int exponent = Print("Введите экспонент: ");
+Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
